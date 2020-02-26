6476f85a9c485af7d62dceb6eee8f52cbf614e6b
nobuti <buti@nobuti.com>
2020-02-26 16:38:21 +0100

    Store person states

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 export default {
   API: '/api',
   TOKEN_LENGTH: 8,
-  TOKEN_EXPIRATION: 5
+  TOKEN_EXPIRATION: 5,
+  PERSON_STATUS: {
+    DISABLED: 'disabled',
+    ENABLED: 'enabled',
+    DELETED: 'deleted'
+  }
 }
